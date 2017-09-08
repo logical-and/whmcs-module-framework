43c58d12032cf8b5fe6299ba8c137c6437c28559
Dmitriy Kachurovskiy <kachurovskiyds@gmail.com>
2017-09-08 17:48:52 +0300

    SendEmail system api method

--- src/Api/System.php
+++ src/Api/System.php
@@ -8,4 +8,33 @@ class System extends AbstractRequest
     {
         return $this->response('getPaymentMethods', [], 'paymentmethods.paymentmethod');
     }
+
+    public function sendEmail(
+        $id = null,
+        $messageName = null,
+        $customSubject = null,
+        $customMessage,
+        $customType = null,
+        array $args = [])
+    {
+        if ($id !== null) {
+            $args['id'] = $id;
+        }
+        if ($messageName !== null) {
+            $args['messagename'] = $messageName;
+        }
+        if ($customType !== null) {
+            $args['customtype'] = $customType;
+        }
+        if ($customSubject !== null) {
+            $args['customsubject'] = $customSubject;
+        }
+        if ($customMessage !== null) {
+            $args['customMessage'] = $customMessage;
+        }
+        if (isset($args['customvars']) && is_array($args['customvars'])) {
+            $args['customvars'] = base64_encode(serialize($args['customvars']));
+        }
+        return $this->response('sendEmail', $args);
+    }
 }
