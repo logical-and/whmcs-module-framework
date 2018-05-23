c791f0631d05c1d8818d8e4c67f3ba1e9d5ba13c
And <and.webdev@gmail.com>
2018-05-23 22:32:43 +0300

    WHMCS 7.5 support - ClonedConnection complete cloning

--- src/ConnectionClone.php
+++ src/ConnectionClone.php
@@ -6,6 +6,7 @@ use
     /** @noinspection PhpUndefinedNamespaceInspection */
     /** @noinspection PhpUndefinedClassInspection */
     Illuminate\Database\Connection;
+use ReflectionObject;
 
 /** @noinspection PhpUndefinedClassInspection */
 class ConnectionClone extends Connection
@@ -14,6 +15,14 @@ class ConnectionClone extends Connection
         /** @noinspection PhpUndefinedClassInspection */
         Connection $conn)
     {
-        return new static($conn->getPdo(), $conn->getDatabaseName(), $conn->getTablePrefix(), $conn->config);
+        $clonedConn = new static($conn->getPdo(), $conn->getDatabaseName(), $conn->getTablePrefix(), $conn->config);
+
+        $reflObj = new ReflectionObject($conn);
+        foreach ($reflObj->getProperties() as $reflProp) {
+            // Works fine since Connection has only protected elements
+            $clonedConn->{$reflProp->getName()} = $conn->{$reflProp->getName()};
+        }
+
+        return $clonedConn;
     }
 }
